ff38ba599a4999fb18533fd6a4d6928e81dfe63f
Scott Kuroda <scott.kuroda@gmail.com>
2012-12-05 01:39:45 -0800

    Prevent sublime from creating autocomplete entries.

--- AdvancedNewFile.py
+++ AdvancedNewFile.py
@@ -22,7 +22,6 @@ NIX_ROOT_REGEX = r"^/"
 
 
 class AdvancedNewFileCommand(sublime_plugin.WindowCommand):
-
     def run(self, is_python=False):
         self.root = None
         self.top_level_split_char = ":"
@@ -167,7 +166,6 @@ class AdvancedNewFileCommand(sublime_plugin.WindowCommand):
 
     def update_filename_input(self, path_in):
         base, path = self.split_path(path_in)
-
         if self.top_level_split_char in path_in:
             PathAutocomplete.set_root(base, False)
         else:
@@ -179,7 +177,6 @@ class AdvancedNewFileCommand(sublime_plugin.WindowCommand):
                     self.generate_creation_path(base, path))
             else:
                 sublime.status_message("Unable to fill status bar without view")
-
         PathAutocomplete.set_path(path)
 
     def generate_creation_path(self, base, path):
@@ -310,11 +307,11 @@ class PathAutocomplete(sublime_plugin.EventListener):
             return []
 
         pac = PathAutocomplete
-        if self.continue_previous_autocomplete():
+        if self.continue_previous_autocomplete() and prefix != "":
             if DEBUG:
                 print "AdvancedNewFile[Debug]: (Prev) Suggestions"
                 print pac.prev_suggestions
-            return pac.prev_suggestions
+            return (pac.prev_suggestions, sublime.INHIBIT_WORD_COMPLETIONS | sublime.INHIBIT_EXPLICIT_COMPLETIONS)
 
         suggestions = []
         suggestions_w_spaces = []
@@ -337,7 +334,6 @@ class PathAutocomplete(sublime_plugin.EventListener):
             sugg, sugg_w_spaces = self.generate_relative_auto_complete(path, base)
             suggestions += sugg
             suggestions_w_spaces += sugg_w_spaces
-
         # If suggestions exist, use complete name
         # else remove base prefix
         if len(suggestions) > 0:
@@ -354,12 +350,11 @@ class PathAutocomplete(sublime_plugin.EventListener):
         pac.prev_base = base
         pac.prev_suggestions = suggestions
         pac.prev_root = root_path
-
         if DEBUG:
             print "AdvancedNewFile[Debug]: Suggestions:"
             print suggestions
 
-        return suggestions
+        return (suggestions, sublime.INHIBIT_WORD_COMPLETIONS | sublime.INHIBIT_EXPLICIT_COMPLETIONS)
 
     def generate_project_auto_complete(self, base):
         folders = sublime.active_window().folders()
@@ -372,6 +367,7 @@ class PathAutocomplete(sublime_plugin.EventListener):
     def generate_auto_complete(self, base, iterable_var):
         sugg = []
         sugg_w_spaces = []
+
         for entry in iterable_var:
             compare_entry = entry
             compare_base = base
