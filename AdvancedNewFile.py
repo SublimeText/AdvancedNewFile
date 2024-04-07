f59f9c7ef770452a590a116deba8f2349062ef07
deathaxe <deathaxe82@googlemail.com>
2024-04-07 19:29:22 +0200

    Drop reloader (#189)
    Fixes #169
    
    Reloading modules is performed by clearing `sys.modules` cache
    before (re-)importing.
    
    That common strategy works well for various packages for several years.

--- AdvancedNewFile.py
+++ AdvancedNewFile.py
@@ -1,22 +1,20 @@
 import sublime
 import sys
 
+# Clear module cache to force reloading all modules of this package.
+
+# kiss-reloader:
+prefix = __package__ + "."  # don't clear the base package
+for module_name in [
+    module_name
+    for module_name in sys.modules
+    if module_name.startswith(prefix) and module_name != __name__
+]:
+    del sys.modules[module_name]
+prefix = None
+
 VERSION = int(sublime.version())
-
-reloader = "advanced_new_file.reloader"
-
 if VERSION > 3000:
-    reloader = 'AdvancedNewFile.' + reloader
-    from imp import reload
-
-
-# Make sure all dependencies are reloaded on upgrade
-if reloader in sys.modules:
-    reload(sys.modules[reloader])
-
-if VERSION > 3000:
-    from .advanced_new_file import reloader
     from .advanced_new_file.commands import *
 else:
-    from advanced_new_file import reloader
     from advanced_new_file.commands import *
